716e3434837895e491b8fd1da9b607da8d7e8a40
Naim <naim@uib.no>
2025-01-28 16:11:18 +0100

    Read number of tx nodes from info.json file

--- training/main.py
+++ training/main.py
@@ -1,9 +1,15 @@
-from config_schema import FullConfig, ModelType, GPUOption
-from train_xgboost import run_sg_xgboost_training
-from train_gnn_based_xgboost import run_sg_embedding_based_xgboost
-from pydantic import ValidationError, TypeAdapter
 import argparse
 import json
+import sys
+import os
+
+
+from pydantic import TypeAdapter, ValidationError
+
+from config_schema import FullConfig, GPUOption, ModelType
+from train_gnn_based_xgboost import run_sg_embedding_based_xgboost
+from train_xgboost import run_sg_xgboost_training
+
 
 def load_config(path: str) -> FullConfig:
     with open(path, 'r') as f:
@@ -49,7 +55,36 @@ if __name__ == "__main__":
         elif user_config.kind == ModelType.GRAPH_SAGE_XGB.value:
             if user_config.gpu == GPUOption.SINGLE.value:
                 #TODO: Read it from meta file
-                num_transaction_nodes=281063
+
+                path_to_gnn_data = os.path.join(configuration.paths.data_dir, "gnn")
+
+                file_containing_nr_tx_nodes = 'info.json'
+                if os.path.exists(path_to_gnn_data):
+                    for file_name in ['edges.csv', 'labels.csv', 'features.csv', file_containing_nr_tx_nodes]:
+                        if not os.path.exists(os.path.join(path_to_gnn_data, file_name)):
+                            sys.exit(f'{file_name} does not exist in {path_to_gnn_data}')
+                else:
+                    sys.exit(f'{path_to_gnn_data} does not exist.')
+
+
+                # Read number of transactions from info.json
+                try:
+                    with open(os.path.join(path_to_gnn_data, file_containing_nr_tx_nodes), 'r') as file:
+                        json_data = json.load(file)
+                except FileNotFoundError:
+                    print(f'Could not find {file_containing_nr_tx_nodes}. Exiting...', file=sys.stderr)
+                    sys.exit(1)
+                except json.JSONDecodeError:
+                    print(f'Invalid JSON in {file_containing_nr_tx_nodes} . Exiting...', file=sys.stderr)
+                    sys.exit(1)
+
+                if "NUM_TRANSACTION_NODES" not in json_data:
+                    print(f'Key NUM_TRANSACTION_NODES not found in {file_containing_nr_tx_nodes}. Exiting...', file=sys.stderr)
+                    sys.exit(1)
+
+                num_transaction_nodes = json_data["NUM_TRANSACTION_NODES"]
+
+                print(f' num_transaction_nodes {num_transaction_nodes}')
 
                 run_sg_embedding_based_xgboost(
                     configuration.paths.data_dir,
